c6f75631d823037f8df623e80de6ec8b5f36450e
Sumit Pandey <sumitpandey7454@gmail.com>
2026-05-30 20:41:50 +0530

    Initial backend setup

---
+++ ats-resume-backend/src/main/java/com/atsresume/AtsResumeBackendApplication.java
@@ -0,0 +1,13 @@
+package com.atsresume;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AtsResumeBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AtsResumeBackendApplication.class, args);
+	}
+
+}
---
+++ ats-resume-backend/src/test/java/com/atsresume/AtsResumeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atsresume;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AtsResumeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
